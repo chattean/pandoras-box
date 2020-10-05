d549d86114dcd8d7731c56ae4d927747e1fc3c7b
Anuj Chatterjee <anujchatterjee@gmail.com>
2020-10-05 03:05:54 -0700

    final commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,33 +4,33 @@ function getRandomInt(n) {
   return Math.floor(Math.random() * n);
 }
 
-// Shuffle the string
+// Shuffle the string so that the the letters chosen in the creteria shouldn't just be at the start. 
 function randomizeString(pass) {
   var arr = pass.split('');           // Convert String to array
   var n = arr.length;              // Length of the array
-  
-  for(var i=0 ; i<n-1 ; ++i) {
+
+  for (var i = 0; i < n - 1; ++i) {
     var j = getRandomInt(n);       // Get random of [0, n-1]
-    
+
     var temp = arr[i];             // Swap arr[i] and arr[j]
     arr[i] = arr[j];
     arr[j] = temp;
   }
-  
+
   pass = arr.join('');                // Convert Array to string
   return pass;                        // Return shuffled string
 }
 
 // password criteria function
-var passwordCriteria = function(){
+var passwordCriteria = function () {
   criteriaConfrim = confirm("Do you want to set a password Criteria?");
   if (criteriaConfrim) {
     criteriaChoice = prompt("Please choose a password criteria to customize your Password:" +
-    "\n1 : Password Length" +
-    "\n2 : Password Character" +
-    "\n3 : Feeling Lazy ... just want us to create a Secure Password with Random values?");
+      "\n1 : Password Length" +
+      "\n2 : Password Character" +
+      "\n3 : Feeling Lazy ... just want us to create a Secure Password with Random values?");
     criteriaChoice = parseInt(criteriaChoice);
-    switch(criteriaChoice){
+    switch (criteriaChoice) {
       case 1:
         //launch the password length function
         LengthOfPassword();
@@ -44,7 +44,7 @@ var passwordCriteria = function(){
         LengthOfPassword();
         break;
       case 3:
-        passwordLength = Math.floor(Math.random() * 128 +8);
+        passwordLength = Math.floor(Math.random() * 128 + 8);
         passwordCriteriaCharacters = "";
         alert("Random values will be assigned to Password Length and random Characters will be chosen");
         break;
@@ -52,58 +52,58 @@ var passwordCriteria = function(){
         alert("You didn't pick a valid option.Try Again.")
         passwordCriteria();
     }
-  }else{
-    passwordLength = Math.floor(Math.random() * 128 +8);
+  } else {
+    passwordLength = Math.floor(Math.random() * 128 + 8);
     passwordCriteriaCharacters = ""
     alert("Random values will be assigned to Password Length and random Characters will be chosen");
   }
 }
 
 // password length function
-var LengthOfPassword = function(){
+var LengthOfPassword = function () {
   // asking user input for the length of the password
   passwordLength = prompt("How long do you want your Password to be? \nPlease pick a number between 8 and 128\n (Default Value : 10)");
   // converting the length to be an integer
   passwordLength = parseInt(passwordLength);
   // Checking if the directions given were followed as in the user picked the right number
-  if (passwordLength<8 || passwordLength>128){
+  if (passwordLength < 8 || passwordLength > 128) {
     alert("You are not following directions, we will ask you again");
     LengthOfPassword();
-  // Making sure that the generate quits gracefully  
-  }else if (!(passwordLength) || passwordLength === "" || passwordLength == null){
+    // Making sure that the generate quits gracefully  
+  } else if (!(passwordLength) || passwordLength === "" || passwordLength == null) {
     confirmCancel = confirm("You either hit cancel or entered no value, Do you want to assign default value : 10?")
-    if (confirmCancel){
+    if (confirmCancel) {
       passwordLength = 10
-    }else{
+    } else {
       LengthOfPassword();
     }
   }
-return passwordLength;
+  return passwordLength;
 }
-var confirmToAddMoreCharfunction = function(){
-  confirmToAddMoreChar= confirm("would you like to add more character types?");
-  if (confirmToAddMoreChar){
+var confirmToAddMoreCharfunction = function () {
+  confirmToAddMoreChar = confirm("would you like to add more character types?");
+  if (confirmToAddMoreChar) {
     passwordCharacter()
-  }else{
+  } else {
     return true;
   }
 }
-var passwordCharacter = function(){
+var passwordCharacter = function () {
 
-  var characterTypeChoice= prompt("Please choose which special character type you would like in your Secure Password:"+
-  "\n1 : LowerCase" +
-  "\n2 : UpperCase" +
-  "\n3 : Numerical" +
-  "\n4 : Special Characters");
+  var characterTypeChoice = prompt("Please choose which special character type you would like in your Secure Password:" +
+    "\n1 : LowerCase" +
+    "\n2 : UpperCase" +
+    "\n3 : Numerical" +
+    "\n4 : Special Characters");
 
   characterTypeChoice = parseInt(characterTypeChoice);
   var confirmToAddMoreChar = true;
-// Case statements will all the Character Types choices : While loops check for Validity of the user input
-  switch(characterTypeChoice){
+  // Case statements will all the Character Types choices : While loops check for Validity of the user input
+  switch (characterTypeChoice) {
     // for lower case char 
     case 1:
       var lowerCaseChar = prompt("Please enter the lower case letter (a,b,c,d...z) that you want in your Secure Password:");
-      while((alphabetLowerCase.indexOf(lowerCaseChar)=== -1) || (lowerCaseChar.length !==1) || (lowerCaseChar === null)){
+      while ((alphabetLowerCase.indexOf(lowerCaseChar) === -1) || (lowerCaseChar.length !== 1) || (lowerCaseChar === null)) {
         lowerCaseChar = prompt("You didn't enter a lower case letter or enterd more than One Character,\nPlease enter the lower case letter (a,b,c,d...z) that you want in your Secure Password:");
       };
       passwordCriteriaCharacters += lowerCaseChar;
@@ -111,7 +111,7 @@ var passwordCharacter = function(){
       break;
     case 2:
       var upperCaseChar = prompt("Please enter the Upper Case letter (A,B,C,D...Z) that you want in your Secure Password:");
-      while((alphabetUpperCase.indexOf(upperCaseChar)=== -1) || (upperCaseChar.length !== 1) || (upperCaseChar === null)){
+      while ((alphabetUpperCase.indexOf(upperCaseChar) === -1) || (upperCaseChar.length !== 1) || (upperCaseChar === null)) {
         upperCaseChar = prompt("You didn't enter a Upper Case letter or entered more than One Character,\nPlease enter the lower case letter  that you want in your Secure Password:");
       };
       passwordCriteriaCharacters += upperCaseChar;
@@ -119,7 +119,7 @@ var passwordCharacter = function(){
       break;
     case 3:
       var numericalChar = prompt("Please enter a Number between 0 and 9 that you want in your Secure Password :");
-      while((numbersChars.indexOf(numericalChar)=== -1)|| (numericalChar.length !== 1)|| (numericalChar === null)){
+      while ((numbersChars.indexOf(numericalChar) === -1) || (numericalChar.length !== 1) || (numericalChar === null)) {
         numericalChar = prompt("You didn't enter a number between 0-9 or enterd more than One Number,\nPlease try again");
       };
       passwordCriteriaCharacters += numericalChar;
@@ -127,7 +127,7 @@ var passwordCharacter = function(){
       break;
     case 4:
       var specialChar = prompt("Please enter the special character that you want in your Secure Password: Here are the acceptable special chars\n" + specialChars);
-      while((specialChars.indexOf(specialChar)=== -1) || (specialChar.length !== 1) || (specialChar === null)){
+      while ((specialChars.indexOf(specialChar) === -1) || (specialChar.length !== 1) || (specialChar === null)) {
         specialChar = prompt("You didn't enter a Special Character or enterd more than One Special Character,\nPlease enter one of these\n" + specialChars)
       };
       passwordCriteriaCharacters += specialChar;
@@ -146,7 +146,7 @@ var alphabetLowerCase = "abcdefghijklmnopqrstuvwxyz";
 // fancy way to get all the uppercase characters
 var alphabetUpperCase = alphabetLowerCase.toUpperCase();
 var numbersChars = "0123456789";
-var specialChars =  "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+var specialChars = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
 var passwordLength = 0;
 var passwordCriteriaCharacters = ""
 var securePassword = "";
@@ -155,12 +155,12 @@ var securePassword = "";
 fullCharacterSet = alphabetLowerCase + alphabetUpperCase + numbersChars + specialChars
 
 //generate password function
-var generatePassword = function(){
+var generatePassword = function () {
   passwordCriteria();
   securePassword += passwordCriteriaCharacters
   // getting a random set of character + the characters that we have already gone over
-  for (var i=0; i<passwordLength;i++){
-    securePassword += fullCharacterSet.charAt(Math.random()* fullCharacterSet.length);
+  for (var i = 0; i < passwordLength; i++) {
+    securePassword += fullCharacterSet.charAt(Math.random() * fullCharacterSet.length);
   }
   securePassword = randomizeString(securePassword)
   return securePassword;
